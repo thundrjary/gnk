c9d2ce0b0e629dbe59e973404ff0aae0e88108b2
Jarom Later <jaromdl@gmail.com>
2025-08-03 23:00:24 -0600

    Update ucsi_acpi.sh

--- system/ucsi_acpi.sh
+++ system/ucsi_acpi.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env bash
+
+set -euo pipefail
+
+if [[ -d "/boot/loader/entries" ]]; then # systemd-boot
+elif [[ -f "/etc/default/grub" ]]; then # grub
+elif [[ -d "/etc/cmdline.d" ]]; then # UKI
+elif [[ -f "/etc/kernel/cmdline" ]]; then # UKI Alt
+else
+        echo ""
+        echo " None of systemd-boot, GRUB, or UKI detected.  Please manually add these kernel parameters:"
+        echo "   - splash (to see the graphical splash screen)"
+        echo "   - quiet (for silent boot)"
+        echo ""
+fi
