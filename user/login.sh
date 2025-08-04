316ea9bce5554d09fc790c79f75b6a1146822d40
Jarom Later <jaromdl@gmail.com>
2025-08-04 09:21:36 -0600

    Update login.sh

--- user/login.sh
+++ user/login.sh
@@ -3,4 +3,5 @@
 yay -S --noconfirm --needed greetd wlgreet
 
 sudo cp config/greetd/config.toml /etc/greetd/config.toml
+sudo cp config/greetd/wlgreet.toml /etc/greetd/wlgreet.toml
 sudo systemctl enable greetd
