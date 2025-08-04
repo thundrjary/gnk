05388eafdb34514d7c266c340f67a7a3d123b16b
Jarom Later <jaromdl@gmail.com>
2025-08-03 22:37:33 -0600

    Create boot.sh

---
+++ boot.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+sudo pacman -Sy --noconfirm --needed git
+
+# handle repo idempotently
+if [[ -d ~/.local/share/gnk ]]; then
+        # directory exists; update if it's a git repo
+        cd ~/.local/share/gnk && git pull --quiet >/dev/null 2>&1
+else
+        git clone https://github.com/thundrjary/gnk.git ~/.local/share/gnk >/dev/null
+fi
+
+# run install script
+source ~/.local/share/gnk/install.sh
