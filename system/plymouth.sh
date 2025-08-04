ba9ac5ac69a9a264b22212598d45ef83f32a284b
Jarom Later <jaromdl@gmail.com>
2025-08-03 23:01:16 -0600

    Create plymouth.sh

---
+++ system/plymouth.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [[ -d "/boot/loader/entries" ]]; then # systemd-boot
+        break
+elif [[ -f "/etc/default/grub" ]]; then # grub
+        break
+elif [[ -d "/etc/cmdline.d" ]]; then # UKI
+        break
+elif [[ -f "/etc/kernel/cmdline" ]]; then # UKI Alt
+        break
+else
+        echo ""
+        echo " None of systemd-boot, GRUB, or UKI detected.  Please manually add these kernel parameters:"
+        echo "   - splash (to see the graphical splash screen)"
+        echo "   - quiet (for silent boot)"
+        echo ""
+fi
