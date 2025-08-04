43a4e2368ada634c0892bc9aea311b03e7887591
Jarom Later <jaromdl@gmail.com>
2025-08-04 02:09:51 -0600

    Create input.sh

---
+++ system/input.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+yay -S --noconfirm --needed \
+    libinput \
+    xf86-input-wacom \
+    libwacom \
+    iio-sensor-proxy \
+
+modprobe elan_i2c || echo "isssue running modprobe elan_ii2c"
